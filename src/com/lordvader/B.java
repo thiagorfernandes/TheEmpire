a71c6dfe39ac082d8d3a0d0b5497b228a44fd52e
Ruan <zero.ruan@gmail.com>
2017-03-05 20:53:27 -0300

    09 classe b criada!

---
+++ src/com/lordvader/B.java
@@ -0,0 +1,20 @@
+package com.lordvader;
+
+public class B {
+
+	private int b1;
+	private float b2;
+	public int getB1() {
+		return b1;
+	}
+	public void setB1(int b1) {
+		this.b1 = b1;
+	}
+	public float getB2() {
+		return b2;
+	}
+	public void setB2(float b2) {
+		this.b2 = b2;
+	}
+	
+}
