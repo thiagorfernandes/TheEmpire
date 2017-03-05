1ce4bee382ccad23c180f7a654b79c55404487e3
Ruan <zero.ruan@gmail.com>
2017-03-05 19:54:29 -0300

    01 Conteúdo incluído!

---
+++ src/com/lordvader/A.java
@@ -0,0 +1,20 @@
+package com.lordvader;
+
+public class A {
+
+	private int a1;
+	private float a2;
+	public int getA1() {
+		return a1;
+	}
+	public void setA1(int a1) {
+		this.a1 = a1;
+	}
+	public float getA2() {
+		return a2;
+	}
+	public void setA2(float a2) {
+		this.a2 = a2;
+	}
+		
+}
