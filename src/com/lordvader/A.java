af564c497b86101a915a35363bbb8055ab4b1504
Ruan <zero.ruan@gmail.com>
2017-03-05 20:10:29 -0300

    02 variavel a3 adicionada!

--- src/com/lordvader/A.java
+++ src/com/lordvader/A.java
@@ -4,6 +4,13 @@ public class A {
 
 	private int a1;
 	private float a2;
+	private String a3;
+	public String getA3() {
+		return a3;
+	}
+	public void setA3(String a3) {
+		this.a3 = a3;
+	}
 	public int getA1() {
 		return a1;
 	}
